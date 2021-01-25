2fcca8c7ad007b3956eb0916e76d1a6b9899b2b6
JH <taerg89@gmail.com>
2021-01-25 23:48:00 +0900

    remove Files

---
+++ JHMemo/JHMemo/Views/Edit/DrawViewController.swift
@@ -0,0 +1,8 @@
+//
+//  DrawViewController.swift
+//  JHMemo
+//
+//  Created by JH on 2021/01/25.
+//
+
+import Foundation
---
+++ JHMemo/JHMemo/Views/Edit/DrawingView.swift
@@ -0,0 +1,8 @@
+//
+//  DrawingView.swift
+//  JHMemo
+//
+//  Created by JH on 2021/01/25.
+//
+
+import Foundation
