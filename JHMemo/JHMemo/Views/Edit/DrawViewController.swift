93d9408363424d9cdbc604ca0b3477ec3ca2d5c2
JH <taerg89@gmail.com>
2021-01-27 00:53:41 +0900

    add Drawing View

--- JHMemo/JHMemo/Views/Edit/DrawViewController.swift
+++ JHMemo/JHMemo/Views/Edit/DrawViewController.swift
@@ -5,4 +5,61 @@
 //  Created by JH on 2021/01/25.
 //
 
-import Foundation
+import UIKit
+
+protocol DrawViewControllerDelegate: class {
+	func getDrawImage(image: UIImage?)
+}
+
+final class DrawViewController: BaseViewController {
+	// MARK: - Properties
+	@IBOutlet private weak var drawingView: DrawingView!
+	@IBOutlet private weak var closeButton: UIButton!
+	@IBOutlet private weak var clearButton: UIButton!
+	@IBOutlet private weak var completButton: UIButton!
+	@IBOutlet private weak var infoLabel: UILabel!
+	@IBOutlet private weak var closeButtonTopContraint: NSLayoutConstraint!
+
+	weak var delegate: DrawViewControllerDelegate?
+
+	// MARK: - Life Cycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		initButton()
+		initEvent()
+		initTextLabel()
+	}
+
+	// MARK: - Initialize
+	private func initButton() {
+		// 상단 높이 조절
+		guard let window = UIWindow.key else { return }
+		closeButtonTopContraint.constant = window.safeAreaInsets.top + 8
+	}
+
+	private func initEvent() {
+		Timer.scheduledTimer(withTimeInterval: 3.0, repeats: true) { timer in
+			self.infoLabel.isHidden = true
+		}
+	}
+
+	private func initTextLabel() {
+		infoLabel.setBodyStyle(text: "이 곳에 메모해주세요.".localized, size: 14)
+	}
+
+	// MARK: - IBAction
+	// 지우기 버튼
+	@IBAction private func actionClear(_ sender: UIButton) {
+		drawingView.clear()
+	}
+
+	@IBAction private func actionComplet(_ sender: UIButton) {
+		dismiss(animated: true, completion: nil)
+		delegate?.getDrawImage(image: self.drawingView.save())
+	}
+
+	@IBAction private func actionClose(_ sender: UIButton) {
+		dismiss(animated: true, completion: nil)
+	}
+}
--- JHMemo/JHMemo/Views/Edit/DrawingView.swift
+++ JHMemo/JHMemo/Views/Edit/DrawingView.swift
@@ -5,4 +5,158 @@
 //  Created by JH on 2021/01/25.
 //
 
-import Foundation
+import UIKit
+
+struct LineInfo {
+	let color: UIColor
+	let lineWidth: CGFloat
+	let line: UIBezierPath
+}
+
+final class DrawingView: UIView {
+	// MARK: - Properties
+	private var drawColor = UIColor.black	// 선 색
+	private var lineWidth: CGFloat = 5   	// 선의 폭
+	private var bezierPath: UIBezierPath = UIBezierPath()
+	private var lastPoint: CGPoint?     	// 마지막 위치를 저장
+	private var pointCounter: Int = 0   	// point 카운터
+	private let pointLimit: Int = 128
+	private var preRenderImage: UIImage?
+	private var allLine: [LineInfo] = []
+	private var cancelLine: [LineInfo] = []
+
+	// MARK: - Initialization
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		initBezierPath()
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		initBezierPath()
+	}
+
+	private func initBezierPath() {
+		lastPoint = .zero
+		bezierPath.lineCapStyle = CGLineCap.round       // 끝점 모양
+		bezierPath.lineJoinStyle = CGLineJoin.round     // 연결 모양
+	}
+	// MARK: - Touch handling
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		let touch: AnyObject? = touches.first
+		guard let currentPoint = touch?.location(in: self) else { return }
+		bezierPath.move(to: currentPoint)
+		lastPoint = currentPoint
+
+		let tmpDic = LineInfo(color: drawColor, lineWidth: lineWidth, line: bezierPath)
+		allLine.append(tmpDic)
+
+		// pointCounter 재설정
+		pointCounter = 0
+	}
+
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		let touch: AnyObject? = touches.first
+		guard let currentPoint = touch?.location(in: self) else { return }
+		let midPoint = self.midPoint(from: lastPoint!, to: currentPoint)
+
+		// bezierPath에 point 추가
+		bezierPath.addQuadCurve(to: midPoint, controlPoint: lastPoint!)
+		lastPoint = currentPoint
+
+		// pointCounter 증가
+		pointCounter += 1
+
+		setNeedsDisplay()
+	}
+
+	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+		// pointCounter 재설정
+		pointCounter = 0
+		renderToImage()
+		setNeedsDisplay()
+
+		// bezierPath 재설정
+		bezierPath.removeAllPoints()
+	}
+
+	override func touchesCancelled(_ touches: Set<UITouch>?, with event: UIEvent?) {
+		touchesEnded(touches!, with: event)
+	}
+
+	// MARK: - Pre render
+	// bezierPath path를 UIImage에 렌더링
+	private func renderToImage() {
+		UIGraphicsBeginImageContextWithOptions(self.bounds.size, false, 0.0)
+		if preRenderImage != nil {
+			preRenderImage?.draw(in: self.bounds)
+		}
+
+		bezierPath.lineWidth = lineWidth
+		drawColor.setFill()
+		drawColor.setStroke()
+		bezierPath.stroke()
+
+		preRenderImage = UIGraphicsGetImageFromCurrentImageContext()
+
+		UIGraphicsEndImageContext()
+	}
+
+	// MARK: - Render
+	override func draw(_ rect: CGRect) {
+		super.draw(rect)
+
+		if preRenderImage != nil {
+			preRenderImage?.draw(in: self.bounds)
+		}
+
+		allLine.forEach {
+			$0.color.setStroke()
+			$0.line.lineWidth = $0.lineWidth
+			$0.line.stroke()
+		}
+	}
+
+	// MARK: - Clearing : 그려진 path들 지움
+	func clear() {
+		preRenderImage = nil
+		bezierPath.removeAllPoints()
+		setNeedsDisplay()
+	}
+
+	func cancel() {
+		bezierPath.close()
+	}
+
+	func save() -> UIImage? {
+		return preRenderImage
+	}
+
+	// MARK: - Other
+	// 선을 확인하는 경우
+	private func hasLines() -> Bool {
+		return preRenderImage != nil || !bezierPath.isEmpty
+	}
+
+	private func midPoint(from point0: CGPoint, to point1: CGPoint) -> CGPoint {
+		let pointX = (point0.x + point1.x) / 2
+		let pointY = (point0.y + point1.y) / 2
+		return CGPoint(x: pointX, y: pointY)
+	}
+
+	private func backImage() {
+		if allLine.isEmpty == false {
+			cancelLine.append(allLine.last!)
+			allLine.removeLast()
+			setNeedsDisplay()
+		}
+	}
+
+	private func forwardImage() {
+		if cancelLine.isEmpty == false {
+			allLine.append(cancelLine.last!)
+			cancelLine.removeLast()
+			setNeedsDisplay()
+		}
+	}
+}
