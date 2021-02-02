fe2faad6308f8b3c362137532be2c6a2cc3cd636
JH <taerg89@gmail.com>
2021-02-03 00:12:29 +0900

    change widget text

--- JHMemo/MemoImageWidgetGroup/MemoImageWidgetGroup.swift
+++ JHMemo/MemoImageWidgetGroup/MemoImageWidgetGroup.swift
@@ -11,22 +11,22 @@ import Intents
 
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+        SimpleEntry(date: Date(), relevance: nil, configuration: ConfigurationIntent())
     }
 
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> Void) {
+        let entry = SimpleEntry(date: Date(), relevance: nil, configuration: configuration)
         completion(entry)
     }
 
-    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
         var entries: [SimpleEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            let entry = SimpleEntry(date: entryDate, relevance: nil, configuration: configuration)
             entries.append(entry)
         }
 
@@ -37,14 +37,21 @@ struct Provider: IntentTimelineProvider {
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
+	let relevance: TimelineEntryRelevance?
     let configuration: ConfigurationIntent
 }
 
 struct MemoImageWidgetGroupEntryView: View {
     var entry: Provider.Entry
 
+	@ViewBuilder
     var body: some View {
-        Text(entry.date, style: .time)
+		Text("오늘은 무슨 일이 있었나요?")
+			.fontWeight(.ultraLight)
+			.font(.system(size: 20))
+			.lineLimit(2)
+			.frame(width: 140, height: 100)
+			.padding()
     }
 }
 
@@ -56,14 +63,15 @@ struct MemoImageWidgetGroup: Widget {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
             MemoImageWidgetGroupEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
+        .configurationDisplayName("지혜로운 메모")
         .description("This is an example widget.")
+		.supportedFamilies([.systemSmall, .systemMedium])
     }
 }
 
 struct MemoImageWidgetGroup_Previews: PreviewProvider {
     static var previews: some View {
-        MemoImageWidgetGroupEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+        MemoImageWidgetGroupEntryView(entry: SimpleEntry(date: Date(), relevance: nil, configuration: ConfigurationIntent()))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
